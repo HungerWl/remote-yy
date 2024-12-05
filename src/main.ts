c6891684a3822a4d4466e87cc2f60795a3bcf6e8
wanli <3203045385@qq.com>
2024-12-05 15:02:56 +0800

    打包测试

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-// src/main.ts
-const appName = import.meta.env.VITE_APP_NAME;
-document.title = appName; // 设置文档标题
-createApp(App).mount('#app')
